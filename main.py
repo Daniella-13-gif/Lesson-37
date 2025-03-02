2e26accc34e6554af7b2f044197c66e2af8cdb31
Daniella-13-gif <ngozihomestore@gmail.com>
2025-03-02 13:21:56 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+name = "Ella" 
+age = 13
+is_student = True
+weight = 40.5 
+
+print("Name:", name)
+print("Data type of name is", type (name))
+
+print("Age:", age)
+print("Data type of age is", type (age))
+
+print("is_student:", is_student)
+print("Data type of name is_student is", type (is_student))
+
+print("Weight:", weight)
+print("Data type of weight is", type (weight))
